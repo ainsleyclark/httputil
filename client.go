c2cec8a58954eed59a893031610ba14e7fb6b9e5
Ainsley Clark <info@ainsleyclark.com>
2022-09-22 08:04:36 +0100

    Added response & updated licence

--- client.go
+++ client.go
@@ -1,6 +1,15 @@
-// Copyright 2020 The Reddico Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
+// Copyright 2022 Ainsley Clark. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 package httputil
 
@@ -25,15 +34,7 @@ type (
 	cycleTLS interface {
 		Do(string, cycletls.Options, string) (cycletls.Response, error)
 	}
-	//Response contains the client response data from
-	// the request.
-	Response struct {
-		RequestID string
-		Status    int
-		Body      string
-		Headers   map[string]string
-		Location  string
-	}
+
 	// httpClient represents the cycleTLS client used
 	// for performing requests.
 	httpClient struct {
